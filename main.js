8f519cb1c5f64e0e0d6a81c249706adec05bfb40
YoKarson <lantk991@gmail.com>
2023-09-24 17:58:49 -0700

    more css

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const loginButton = document.getElementById('login-btn');
-const logoutButton = document.getElementById('logout-btn');
-
-loginButton.onclick = loginFunction;
-logoutButton.onclick = logoutFunction;
-
-function loginFunction() {
-    window.location.href = 'home.html';
-}
-
-function logoutFunction() {
-    window.location.href = 'index.html';
-}
+    const loginButton = document.getElementById('login-btn');
+    const logoutButton = document.getElementById('logout-btn');
+    
+    loginButton.onclick = loginFunction;
+    logoutButton.onclick = logoutFunction;
+    
+    function loginFunction() {
+      window.location.href = 'home.html';
+    }
+    
+    function logoutFunction() {
+      window.location.href = 'index.html';
+    }
